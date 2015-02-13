a9713863faf4bbc4e106860d694d3cc21236b32d
Robert Clipsham <robert@octarineparrot.com>
2015-02-13 16:09:32 +0000

    Fixes for latest Rust master.

--- src/lib.rs
+++ src/lib.rs
@@ -102,7 +102,7 @@
 #![deny(missing_docs)]
 
 // FIXME Remove this once the std lib has stabilised
-#![feature(collections, core, env, io, libc, os, std_misc)]
+#![feature(alloc, core, collections, env, io, libc, os, std_misc)]
 
 extern crate libc;
 
