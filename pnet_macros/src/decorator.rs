9177687ebf7b7cb017eda3ba047169daa4320ad2
Linus Färnstrand <faern@faern.net>
2016-09-15 20:17:14 +0200

    Improve documentation on new constructor

--- pnet_macros/src/decorator.rs
+++ pnet_macros/src/decorator.rs
@@ -931,7 +931,8 @@ fn generate_packet_impl(cx: &mut GenContext,
         }}
 
         /// Constructs a new {name}. If the provided buffer is less than the minimum required
-        /// packet size, this will return None.
+        /// packet size, this will return None. With this constructor the {name} will
+        /// own its own data and the underlying buffer will be dropped when {name} is.
         pub fn owned(packet: Box<[u8]>) -> Option<{name}<'static>> {{
             if packet.len() >= {name}::minimum_packet_size() {{
                 use ::pnet::packet::{cap_mut}PacketData;
