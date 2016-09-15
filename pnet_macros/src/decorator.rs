d6d621a061f1c35b06f69ed229f182ed47cfc75b
Linus Färnstrand <faern@faern.net>
2016-09-15 22:56:47 +0200

    Remove uneccessary match

--- pnet_macros/src/decorator.rs
+++ pnet_macros/src/decorator.rs
@@ -946,9 +946,7 @@ fn generate_packet_impl(cx: &mut GenContext,
         #[inline]
         pub fn to_immutable<'p>(&'p self) -> {imm_name}<'p> {{
             use ::pnet::packet::PacketData;
-            match *self {{
-                {name} {{ ref packet }} => {imm_name} {{ packet: PacketData::Borrowed(packet.as_slice()) }}
-            }}
+            {imm_name} {{ packet: PacketData::Borrowed(self.packet.as_slice()) }}
         }}
 
         /// Maps from a {name} to a {imm_name} while consuming the source
