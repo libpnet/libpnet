81622f6319af86d3d12ebaeeedd407f141058ede
Linus Färnstrand <faern@faern.net>
2016-10-30 22:03:17 +0100

    Rustfmt (with manual adjustments) on pnet_macros

--- pnet_macros/tests/run-pass/variable_length_fields.rs
+++ pnet_macros/tests/run-pass/variable_length_fields.rs
@@ -46,10 +46,7 @@ fn main() {
         p.set_data(&vec![0x0001, 0x1223, 0x3ff4]);
     }
 
-    let ref_packet = [0x06, /* length */
-                      0x00, 0x01,
-                      0x12, 0x23,
-                      0x3f, 0xf4];
+    let ref_packet = [0x06 /* length */, 0x00, 0x01, 0x12, 0x23, 0x3f, 0xf4];
 
     assert_eq!(&ref_packet[..], &packet[..]);
 
