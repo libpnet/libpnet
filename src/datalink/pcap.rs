db80a9d333aeca3b0f43cfb494f889b5b152ddd8
Daniel Svensson <dsvensson@gmail.com>
2017-10-06 03:36:28 +0200

    Update pcap datalink to pcap API changes introduced in 0.6.0.
    pcap#57b6d912dfd3ebc38f778af7f1b24e1b26855589

--- src/datalink/pcap.rs
+++ src/datalink/pcap.rs
@@ -108,7 +108,7 @@ impl EthernetDataLinkSender for DataLinkSenderImpl {
                 func(eh);
             }
             let mut cap = self.capture.lock().unwrap();
-            if let Err(e) = cap.sendpacket(&data) {
+            if let Err(e) = cap.sendpacket(data) {
                 return Some(Err(io::Error::new(io::ErrorKind::Other, e)))
             }
         }
