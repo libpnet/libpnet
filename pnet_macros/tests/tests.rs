bb1e454402e253e5e39b60e5c84881ff84f83bdc
Pierre Chifflier <chifflier@wzdftpd.net>
2021-05-10 10:20:00 +0200

    pnet_packet: switch tests to trybuild

--- pnet_macros/tests/tests.rs
+++ pnet_macros/tests/tests.rs
@@ -1,22 +1,6 @@
-extern crate compiletest_rs as compiletest;
-extern crate pnet_macros;
-
-use compiletest::Config;
-use std::path::PathBuf;
-
-fn run_mode(mode: &'static str) {
-    let mut config = Config::default();
-
-    config.mode = mode.parse().expect("Invalid mode");
-    config.src_base = PathBuf::from(format!("tests/{}", mode));
-    config.link_deps(); // Populate config.target_rustcflags with dependencies on the path
-    config.clean_rmeta(); // If your tests import the parent crate, this helps with E0464
-
-    compiletest::run_tests(&config);
-}
-
 #[test]
 fn compile_test() {
-    run_mode("compile-fail");
-    run_mode("run-pass");
+    let t = trybuild::TestCases::new();
+    t.compile_fail("tests/compile-fail/*.rs");
+    t.pass("tests/run-pass/*.rs");
 }
